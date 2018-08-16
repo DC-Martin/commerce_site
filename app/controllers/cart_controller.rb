72bdf9e2743fb6551c0b3aecd945b2b32767087f
Chase Martin <chasemartin88@gmail.com>
2018-08-16 09:52:08 -0400

    added admin capabilities

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,7 @@
 class CartController < ApplicationController
+
+  before_action :authenticate_user!, except: [:add_to_cart, :view_order]
+
   def add_to_cart
     check = LineItem.find_by(product_id: params[:product_id])
     if check
@@ -15,6 +18,26 @@ class CartController < ApplicationController
     @line_items = LineItem.all
   end
 
+  def cart_edit
+    
+  end
+
+  def cart_delete
+    
+  end
+
   def checkout
+    line_items = LineItem.all
+    @order = Order.create(user_id: current_user.id, subtotal: 0)
+    line_items.each do |item|
+      item.product.update(quantity: (item.product.quantity - item.quantity))
+      @order.order_items[item.product_id] = item.quantity
+      @order.subtotal += item.line_item_total
+    end
+    @order.save
+    @order.update(sales_tax: (@order.subtotal * 0.08))
+    @order.update(grand_total: (@order.sales_tax + @order.subtotal))
+
+    line_items.destroy_all
   end
 end
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,4 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  validates :quantity, numericality: {greater_than: 0}
 end
